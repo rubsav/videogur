c418dccd6a9c39f29cf9ebfc340dce81e26e883d
Ruben Avila <Ruavilaa@hotmail.com>
2014-11-06 13:06:03 -0500

    Adding feature to edit videos

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -12,21 +12,36 @@ class VideosController < ApplicationController
 		@video = Video.new
 	end
 
-	 def create
+	def create
     # make a new video with what video_params returns (which is a method we're calling)
-    @video = video.new(video_params)
-    if @video.save
+    	@video = video.new(video_params)
+    		if @video.save
       # if the save for the video was successful, go to index.html.erb
-      redirect_to videos_url
-    else
+      			redirect_to videos_url
+   			else
       # otherwise render the view associated with the action :new (i.e. new.html.erb)
-      render :new
-    end
-  end
+      			render :new
+    		end
+  	end
+
+  	def edit
+    	@video = video.find(params[:id])
+  	end
+
+  	def update
+    	@video = video.find(params[:id])
+
+    		if @video.update_attributes(video_params)
+      			redirect_to "/videos/#{@video.id}"
+    		else
+      			render :edit
+    		end
+  	end
+
 
   private
-  def video_params
-    params.require(:video).permit(:artist, :title, :url)
-  end
+  	def video_params
+    	params.require(:video).permit(:artist, :title, :url)
+  	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   post 'videos' => 'videos#create'
   get "videos/new" => "videos#new"
   get 'videos/:id' => 'videos#show', as: 'video'
+  get 'videos/:id/edit' => "videos#edit", as: "edit_video"
+  patch 'videos/:id' => "videos#update"
 end
