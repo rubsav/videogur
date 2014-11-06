264b9d5f69e896d3b69ce1e829887d6467e899ce
Ruben Avila <Ruavilaa@hotmail.com>
2014-11-05 23:30:43 -0500

    Adding feature to create new video using HTML form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'videos' => 'videos#index'
+  post 'videos' => 'videos#create'
   get "videos/new" => "videos#new"
   get 'videos/:id' => 'videos#show', as: 'video'
 end
