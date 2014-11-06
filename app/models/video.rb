5904e97aa6d77e5322da646f79e35278288b96c2
Ruben Avila <Ruavilaa@hotmail.com>
2014-11-06 17:28:11 -0500

    Add scopes.

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,9 +2,9 @@ class Video < ActiveRecord::Base
 	scope :newest_first, -> { order(“created_at DESC”) }
 	scope :most_recent_five, -> { newest_first.limit(5) }
 	
-  def self.created_before(time)
-    where("created_at < ?", time) 
-  end
+  # def self.created_before(time)
+  #   where("created_at < ?", time) 
+  # end
 	# this class method is equivalent to:
-	# scope :created_before, ->(time) { where("created_at < ?", time) }
+	scope :created_before, ->(time) { where("created_at < ?", time) }
 end
